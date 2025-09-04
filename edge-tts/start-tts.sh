f2b58876a2c59d8630170c9052f3311e2ccc0913
HackTestes <caio.hacktestes@gmail.com>
2025-09-04 17:02:14 -0300

    Fixed the ffmpeg command

--- edge-tts/start-tts.sh
+++ edge-tts/start-tts.sh
@@ -1,6 +1,9 @@
 # Limit the amount of jobs so the client doesn't get flagged/mistaken for DoS
 MAX_JOBS=10
 
+# Chnage to the texts directory
+cd ~/texts &&
+
 # Get all the text files and transform them info .mp3 files (in other words, run the tts)
 # See: https://stackoverflow.com/a/3894463
 ls *.txt | sed 's/\..*//' | xargs -i --max-procs=$MAX_JOBS bash -c "edge-tts --voice pt-BR-FranciscaNeural --file {}.txt --write-media {}.mp3" &&
@@ -10,5 +13,5 @@ ls *.txt | sed 's/\..*//' | xargs -i --max-procs=$MAX_JOBS bash -c "edge-tts --v
 # ffmpeg explanation
 # -f fmt: select format/input device
 # -i input: input data
-# -shortest: Finish encoding when the shortest input stream ends
-ls *.mp3 | sed 's/\..*//' | xargs -i --max-procs=0 bash -c "ffmpeg -f lavfi -i color=c=black:s=192x144 -i {}.txt -shortest -fflags +shortest {}.mp3"
+# -shortest: Finish encoding when the shortest input stream ends (if this is not used, the output grows infinitely)
+ls *.mp3 | sed 's/\..*//' | xargs -i --max-procs=0 bash -c "ffmpeg -f lavfi -i color=c=black:s=192x144 -i {}.mp3 -y -shortest {}.mp4"
