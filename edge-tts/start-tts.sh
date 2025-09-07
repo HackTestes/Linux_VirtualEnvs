6954f4d45d3920c95ccc02f2f0f7028c35351496
HackTestes <caio.hacktestes@gmail.com>
2025-09-07 18:14:47 -0300

    Fixed the ffmpeg command: reduced lavfi quality; added AV1 encoding; mp3 for audio encoding

--- edge-tts/start-tts.sh
+++ edge-tts/start-tts.sh
@@ -11,7 +11,16 @@ ls *.txt | sed 's/\..*//' | xargs -i --max-procs=$MAX_JOBS bash -c "edge-tts --v
 # Transform all the mp3 files into .mp4
 # Since this is all local, there is no need to limit the amount of jobs
 # ffmpeg explanation
-# -f fmt: select format/input device
-# -i input: input data
-# -shortest: Finish encoding when the shortest input stream ends (if this is not used, the output grows infinitely)
-ls *.mp3 | sed 's/\..*//' | xargs -i --max-procs=0 bash -c "ffmpeg -f lavfi -i color=c=black:s=192x144 -i {}.mp3 -y -shortest {}.mp4"
+# -f lavfi -> device that generates the input (the color) - see: https://www.ffmpeg.org/ffmpeg-filters.html#allrgb_002c-allyuv_002c-color_002c-colorchart_002c-colorspectrum_002c-haldclutsrc_002c-nullsrc_002c-pal75bars_002c-pal100bars_002c-rgbtestsrc_002c-smptebars_002c-smptehdbars_002c-testsrc_002c-testsrc2_002c-yuvtestsrc
+# color=c=black:s=1x1:r=1 -> color as black; resolution 1x1; fps to 1 (all to improve speed)
+# -f fmt -> select format/input device
+# -i input -> input data
+# -c:v libaom-av1 -> use AV1 for encoding the video
+# -c:a mp3 -> use mp3 to encode the audio
+# -preset slow -> trade-off between compression and performance
+# -r 1 -> output spf set to 1
+# -shortest -> Finish encoding when the shortest input stream ends (if this is not used, the output grows infinitely)
+ls *.mp3 | sed 's/\..*//' | xargs -i --max-procs=0 bash -c "echo 'Generating {}.mp4...' && ffmpeg -f lavfi -i color=c=black:s=1x1:r=1 -i {}.mp3 -preset slow -y -shortest -c:v libaom-av1 -c:a mp3 -r 1 {}.mp4 && echo 'Finished {}.mp4...'"
+
+# TEST COMMAND
+# ffmpeg -f lavfi -i color=c=black:s=1x1:r=1 -i SistemaNacionalDeUnidadesDeConservacaoDaNatureza_9985_2000_altered.mp3 -preset slow -y -shortest -c:v libaom-av1 -c:a mp3 -r 1 SistemaNacionalDeUnidadesDeConservacaoDaNatureza_9985_2000_altered.mp4
