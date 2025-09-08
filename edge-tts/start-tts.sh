83c8346af67df72e0312d1b7663b1daec08c6a46
HackTestes <caio.hacktestes@gmail.com>
2025-09-08 12:21:31 -0300

    Changed the video codec AV1 -> H264 (and resolution change); and audio codec to just copy the original

--- edge-tts/start-tts.sh
+++ edge-tts/start-tts.sh
@@ -6,7 +6,7 @@ cd ~/texts &&
 
 # Get all the text files and transform them info .mp3 files (in other words, run the tts)
 # See: https://stackoverflow.com/a/3894463
-ls *.txt | sed 's/\..*//' | xargs -i --max-procs=$MAX_JOBS bash -c "edge-tts --voice pt-BR-FranciscaNeural --file {}.txt --write-media {}.mp3" &&
+ls *.txt | sed 's/\..*//' | xargs -i --max-procs=$MAX_JOBS bash -c "Generating {}.mp3...' && edge-tts --voice pt-BR-FranciscaNeural --file {}.txt --write-media {}.mp3 && echo 'Finished {}.mp3...'" &&
 
 # Transform all the mp3 files into .mp4
 # Since this is all local, there is no need to limit the amount of jobs
@@ -15,12 +15,12 @@ ls *.txt | sed 's/\..*//' | xargs -i --max-procs=$MAX_JOBS bash -c "edge-tts --v
 # color=c=black:s=1x1:r=1 -> color as black; resolution 1x1; fps to 1 (all to improve speed)
 # -f fmt -> select format/input device
 # -i input -> input data
-# -c:v libaom-av1 -> use AV1 for encoding the video
-# -c:a mp3 -> use mp3 to encode the audio
+# -c:v libopenh264 -> use H264 for encoding the video
+# -c:a copy -> just copy the audio (lossless and increases speed a lot)
 # -preset slow -> trade-off between compression and performance
 # -r 1 -> output spf set to 1
 # -shortest -> Finish encoding when the shortest input stream ends (if this is not used, the output grows infinitely)
-ls *.mp3 | sed 's/\..*//' | xargs -i --max-procs=0 bash -c "echo 'Generating {}.mp4...' && ffmpeg -f lavfi -i color=c=black:s=1x1:r=1 -i {}.mp3 -preset slow -y -shortest -c:v libaom-av1 -c:a mp3 -r 1 {}.mp4 && echo 'Finished {}.mp4...'"
+ls *.mp3 | sed 's/\..*//' | xargs -i --max-procs=0 bash -c "ffmpeg -f lavfi -i color=c=black:s=16x16:r=1 -i {}.mp3 -preset slow -y -shortest -c:v libopenh264 -c:a copy -r 1 {}.mp4"
 
 # TEST COMMAND
 # ffmpeg -f lavfi -i color=c=black:s=1x1:r=1 -i SistemaNacionalDeUnidadesDeConservacaoDaNatureza_9985_2000_altered.mp3 -preset slow -y -shortest -c:v libaom-av1 -c:a mp3 -r 1 SistemaNacionalDeUnidadesDeConservacaoDaNatureza_9985_2000_altered.mp4
